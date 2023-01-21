86cd26e929081fcd28e66cc2edf96e30701590d4
olivier chicheportiche <olivier.chicheportiche@gmail.com>
2023-01-21 20:53:07 +0100

    design

---
+++ front/src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './style.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   
+    <div className="container">
+    <div className="header"><h1>My Space Journey</h1></div>
+    <div className="footer"></div>
+    <div className="register">
+      <div className="gif"></div>
+      <div className="button">
+          <div className="register-b">
+          <button>REGISTER</button>
+          </div>
+          <div className="login-b">
+            <button>LOGIN</button>
+          </div>
+          <div className="alert">
+            <div className='c1'></div>
+            <div className='c2'></div>
+            <div className='c3'></div>
+            <div className='c4'></div>
+            <div className='c5'></div>
+            <div className='c6'></div>
+            <div className='c7'></div>
+            <div className='c8'></div>
+            <div className='c9'></div>
+          </div>
+          <div className="topgallery">
+          <button>TOP GALLERY</button>
+          </div>
+        <div className="alert2">
+          <button>ALERT</button>
+        </div>
+        <div className="alert3"><button>« That’s one small step for man, one giant leap for mankind ».</button></div>
+      </div>
+      <div className="col1"></div>
+      <div className="col2"></div>
+    </div>
+    <div className="photo"><p>Nasa pic of the day</p></div>
+  </div>
+  )
+}
+
+export default App
